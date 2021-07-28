40b486bd2f21d18907677d8924f65826fb1b1954
Taku <Takuya.K.Toyokawa@gmail.com>
2021-07-28 13:40:56 -0700

    Optimized images and fixed round button bug.

--- pages/index.js
+++ pages/index.js
@@ -46,21 +46,21 @@ export default function Home() {
       {/* loading animation */}
       <div
         id="home"
-        class="loading absolute flex flex-col w-screen h-screen justify-center items-center z-40"
+        className="loading absolute flex flex-col w-screen h-screen justify-center items-center z-40"
       >
-        <img class="w-64" src="/logos/ghibli_logo.png"></img>
-        <h1 class="text-gray-800 text-xl pt-6 font-bold">Ghibli Films.</h1>
+        <img className="w-64" src="/logos/ghibli_logo.png"></img>
+        <h1 className="text-gray-800 text-xl pt-6 font-bold">Ghibli Films.</h1>
         <p>Your one stop for all classic Ghibli films.</p>
       </div>
-      <main class="background flex flex-col justify-center bg-gray-50">
-        <div class="flex justify-center my-8">
-          <img class="w-64 invert-0" src="/logos/ghibli_logo.png" />
+      <main className="background flex flex-col justify-center bg-gray-50">
+        <div className="flex justify-center my-8">
+          <img className="w-64 invert-0" src="/logos/ghibli_logo.png" />
         </div>
-        <div class="flex flex-col fixed w-full md:w-auto h-24 z-20 bottom-0 md:top-10 md:right-10 mb-8 items-center md:items-end">
-          <p class="font-bold bg-white p-2 rounded-xl shadow-md mb-3 md:pb-3 md:bg-transparent md:shadow-none">
+        <div className="flex flex-col fixed w-full md:w-auto h-24 z-20 bottom-0 md:top-10 md:right-10 mb-8 items-center md:items-end">
+          <p className="font-bold bg-white p-2 rounded-xl shadow-md mb-3 md:pb-3 md:bg-transparent md:shadow-none">
             Sort By...
           </p>
-          <div class="flex flex-row justify-center md:justify-none md:w-auto">
+          <div className="flex flex-row justify-center md:justify-none md:w-auto">
             <Button
               onClick={SortByReleaseDate}
               icon="/calendar.svg"
@@ -78,16 +78,17 @@ export default function Home() {
             ></Button>
           </div>
         </div>
-        <div class="fixed top-10 sm:bottom-7 right-10 z-20">
+        <div className="fixed top-10 sm:bottom-7 sm:top-auto right-10 z-20">
           <RoundButton href="#home" icon="/cheveron-up.svg" />
         </div>
-        <div class="flex flex-col w-full z-10">
-          <div class="flex justify-center w-full ">
-            <div class="flex flex-wrap w-5/6">
+        <div className="flex flex-col w-full z-10">
+          <div className="flex justify-center w-full ">
+            <div className="flex flex-wrap w-5/6">
               {films.map((o, i) => {
                 return (
-                  <div class="w-full md:w-1/2 p-4">
+                  <div key={i} className="w-full md:w-1/2 p-4">
                     <Card
+                      key={i}
                       release_date={o.release_date}
                       title={o.title}
                       description={o.description}
@@ -99,9 +100,9 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div class="h-12"></div>
+        <div className="h-12"></div>
         <BackgroundAnimation />
-        <img class="fixed bottom-0 w-full" src="/mountain.png" />
+        <img className="fixed bottom-0 w-full" src="/mountain.png" />
       </main>
     </>
   );
